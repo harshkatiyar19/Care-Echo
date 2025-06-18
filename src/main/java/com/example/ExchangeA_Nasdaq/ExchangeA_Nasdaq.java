b1029285fb243e123ca2c7249ea82bb6e56d8ba3
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-06-18 19:35:56 +0530

    init

--- src/main/java/com/example/ExchangeA_Nasdaq/ExchangeA_Nasdaq.java
+++ src/main/java/com/example/ExchangeA_Nasdaq/ExchangeA_Nasdaq.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CareEchoApplication {
+public class ExchangeA_Nasdaq {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CareEchoApplication.class, args);
+		SpringApplication.run(ExchangeA_Nasdaq.class, args);
 	}
 
 }
