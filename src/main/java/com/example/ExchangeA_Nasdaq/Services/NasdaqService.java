bdc21332a854bc948b737489e8fdca596728cc94
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-06-20 07:42:19 +0530

    init

--- src/main/java/com/example/ExchangeA_Nasdaq/Services/NasdaqService.java
+++ src/main/java/com/example/ExchangeA_Nasdaq/Services/NasdaqService.java
@@ -383,7 +383,7 @@ public class NasdaqService {
         }
 
         template.convertAndSend("/topic/order", book);
-        System.out.println("Sent: " + book);
+//        System.out.println("Sent: " + book);
         scheduleNextRun();
     }
 
